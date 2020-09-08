0f566020e9d4cb39dda0c32a485d0b5b6709f240
Nomfundo Verah Sibiya <nvsibiya0@gmail.com>
2020-09-08 14:13:05 +0200

    portfolio

---
+++ build/precache-manifest.37a8b77bc7b8a89e13a13f5524553f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b8df43bbab5d1b84214897bdc543d6",
+    "url": "/nvsibiya/react-portfolio-masterindex.html"
+  },
+  {
+    "revision": "b3931a40543f1b7c781c",
+    "url": "/nvsibiya/react-portfolio-masterstatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/nvsibiya/react-portfolio-masterstatic/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/nvsibiya/react-portfolio-masterstatic/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3931a40543f1b7c781c",
+    "url": "/nvsibiya/react-portfolio-masterstatic/js/main.4ca79ebe.chunk.js"
+  },
+  {
+    "revision": "9f5f02e565150e5a3d06",
+    "url": "/nvsibiya/react-portfolio-masterstatic/js/runtime-main.ad332b61.js"
+  }
+]);
